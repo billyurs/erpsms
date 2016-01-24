f114a66a8969337bd9fc52c25f915d26cc7a8fb0
madhusudan.d <madhusudan.d@goibibo.com>
2016-01-24 22:20:36 +0530

    Added Login

--- erpsms/settings.py
+++ erpsms/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social.apps.django_app.default',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -99,3 +100,23 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+TEMPLATE_CONTEXT_PROCESSORS = (
+   'django.contrib.auth.context_processors.auth',
+   'django.core.context_processors.debug',
+   'django.core.context_processors.i18n',
+   'django.core.context_processors.media',
+   'django.core.context_processors.static',
+   'django.core.context_processors.tz',
+   'django.contrib.messages.context_processors.messages',
+   'social.apps.django_app.context_processors.backends',
+   'social.apps.django_app.context_processors.login_redirect',
+)
+AUTHENTICATION_BACKENDS = (
+   'social.backends.facebook.FacebookOAuth2',
+   'social.backends.google.GoogleOAuth2',
+   'social.backends.twitter.TwitterOAuth',
+   'django.contrib.auth.backends.ModelBackend',
+)
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = '/'
+#SOCIAL_AUTH_FACEBOOK_KEY = 
--- erpsms/urls.py
+++ erpsms/urls.py
@@ -2,24 +2,25 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView, TemplateView
 from django.contrib import admin
 admin.autodiscover()
-import settings
-
 import os
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 STATIC_PATH = PROJECT_PATH
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'erpsms.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       #url(r'^$', 'erpsms.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+                       url(r'^admin/', include(admin.site.urls)),
+                       #url(r'^$', TemplateView.as_view(template_name="index.html")),
 
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_PATH+"/static/" }),
-    url(r'^(?P<path>.*\.txt)$','django.views.static.serve', {'document_root': STATIC_PATH+"/"}),
-    url(r'^/static/avatars/(?P<path>.*)$', 'django.views.static.serve',
-                           {'document_root': STATIC_PATH + '/static/avatars/',}),
-
-
-)
-
+                       url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': STATIC_PATH + "/static/"}),
+                       url(r'^(?P<path>.*\.txt)$', 'django.views.static.serve',
+                           {'document_root': STATIC_PATH + "/"}),
+                       url(r'^/static/avatars/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': STATIC_PATH + '/static/avatars/', }),
+                       url('', include(
+                           'social.apps.django_app.urls', namespace='social')),
+                       url(r'^$', 'erpsms.views.home', name='home'),
+                       url('', include('django.contrib.auth.urls', namespace='auth')),
+                       )
