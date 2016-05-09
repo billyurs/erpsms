f9a40cc844b694a7722e59e110dd1c2f7de87ef4
madhusudan.d <madhusudan.d@goibibo.com>
2016-05-09 21:03:00 +0530

    proto

--- common/communicators.py
+++ common/communicators.py
@@ -2,5 +2,9 @@ from django.core.mail import send_mail, EmailMessage
 
 def sendemail(email_subject,email_body,from_email,to_email,attchment, attachment = []):
 	if attachment:
-		pass
+		mail = EmailMessage(email_subject, email_body, from_email, to_email)
+		for attachfile in attachment:
+	        attachfile.
+	else:
+		send_mail(email_subject,email_body,from_email,to_email,attchment,fail_silently=False)
 	pass
