99104821c18391850a79f81c8e6d40bc4ac42cd2
madhusudan.d <madhusudan.d@goibibo.com>
2016-05-11 20:23:01 +0530

    Communicator

--- common/communicators.py
+++ common/communicators.py
@@ -1,10 +1,14 @@
 from django.core.mail import send_mail, EmailMessage
+import settings
+default_from_email = settings.DEFAULT_FROM_EMAIL
 
-def sendemail(email_subject,email_body,from_email,to_email,attchment, attachment = []):
-	if attachment:
-		mail = EmailMessage(email_subject, email_body, from_email, to_email)
-		for attachfile in attachment:
-	        attachfile.
-	else:
-		send_mail(email_subject,email_body,from_email,to_email,attchment,fail_silently=False)
-	pass
+def sendemail(email_subject, email_body, from_email, to_email, attachment = []):
+    if not from_email:
+        from_email = default_from_email
+    if attachment:
+        mail = EmailMessage(email_subject, email_body, from_email, to_email)
+        for attachfile in attachment:
+            mail.attach(attachfile, mail.read(), mail.content_type)
+        mail.send()
+    else:
+        send_mail(email_subject, email_body, from_email, to_email, fail_silently=False)
