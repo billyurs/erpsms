fbe29a0b001aff58404732c79eff3a366c3c9276
madhusudan.d <madhusudan.d@goibibo.com>
2016-05-12 16:11:26 +0530

    Cron task fixed

--- common/communicators.py
+++ common/communicators.py
@@ -1,8 +1,20 @@
 from django.core.mail import send_mail, EmailMessage
 from  erpsms.settings import DEFAULT_FROM_EMAIL
+import logging
+
+logger_stats = logging.getLogger('erpsms_stats')
 default_from_email = DEFAULT_FROM_EMAIL
 
-def sendemail(email_subject, email_body, from_email, to_email, attachment = []):
+def sendemail(email_subject, email_body, from_email, to_email, attachment=[]):
+    """
+    API to send the email
+    :param email_subject:
+    :param email_body:
+    :param from_email:
+    :param to_email:
+    :param attachment:
+    :return:
+    """
     if not from_email:
         from_email = default_from_email
     if attachment:
@@ -12,3 +24,5 @@ def sendemail(email_subject, email_body, from_email, to_email, attachment = []):
         mail.send()
     else:
         send_mail(email_subject, email_body, from_email, to_email, fail_silently=False)
+    logger_stats.info('Email Subject:%s\tEmail Body:%s\tFrom Email:%s\tTo Email%s\t attachement %s' % (
+    email_subject, email_body, from_email, to_email, attachment))
--- erpsms/tasks.py
+++ erpsms/tasks.py
@@ -4,16 +4,18 @@ import datetime
 import settings
 import os
 def dailyLogReport():
-	import pdb;  pdb.set_trace()
 	workingdir = os.getcwd()
 	# Attach erpsms, erpsms_stats
 	erpsmsfilepath = '%s/erpsms/logs/erpsms.log'%(workingdir)
+	erpsmsfilepath = '' if not os.path.isfile(erpsmsfilepath) else erpsmsfilepath
 	erpsms_statsfilepath = '%s/erpsms/logs/erpsms_stats.log'%(workingdir)
+	erpsms_statsfilepath = '' if not os.path.isfile(erpsms_statsfilepath) else erpsmsfilepath
 	emailsub = 'Daily log report [erpms, erpsms_stats]'
 	DateTime = ('Current Datetime %s'%(datetime.datetime.now()))
-	emailbody = '<html><head>Report Generated DateTime: %s </head></html>'%(DateTime)
+	emailbody = 'Report Generated DateTime: %s'%(DateTime)
 	to_mail = settings.DEFAULT_FROM_EMAIL
 	attachmentlist = [erpsmsfilepath,erpsms_statsfilepath]
 	sendemail(emailsub,emailbody,'',to_mail,attachmentlist)
 
-	
+if __name__ == "__main__":
+	dailyLogReport()
